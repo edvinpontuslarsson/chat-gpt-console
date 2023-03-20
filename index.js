c6affb549a1e6845e13c543b04ee02f5bbf9e810
Edvin Larsson <edvinpontuslarsson@gmail.com>
2023-03-20 15:37:23 +0100

    talks with chat-gpt

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 "use strict";
 
-console.log("How can I help you today?");
+require("dotenv").config();
+const { Configuration, OpenAIApi } = require("openai");
+
+console.log("Please submit your message to chat-gpt:\n\n");
 
 const stdin = process.openStdin();
 
 // gets called on console input
-stdin.addListener("data", (data) => {
-  const input = data.toString().trim();
-  console.log("You said", input);
+stdin.addListener("data", async (data) => {
+  const apiKey = process.env.CHAT_GPT_API_KEY;
+  const message = data.toString().trim();
+
+  const response = await askChatGPTAPI(apiKey, message);
+  console.log(response);
 });
+
+/**
+ * @param {string} apiKey
+ * @param {string} message
+ * @returns {string}
+ */
+async function askChatGPTAPI(apiKey, message) {
+  const configuration = new Configuration({ apiKey });
+  const openai = new OpenAIApi(configuration);
+
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: message,
+  });
+
+  return completion.data.choices[0].text;
+}
