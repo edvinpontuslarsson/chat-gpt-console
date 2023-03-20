3ecd5582ee4b0a22a73093aee89eb52b59060fbc
Edvin Larsson <edvinpontuslarsson@gmail.com>
2023-03-20 15:45:02 +0100

    adds model option

--- index.js
+++ index.js
@@ -3,32 +3,31 @@
 require("dotenv").config();
 const { Configuration, OpenAIApi } = require("openai");
 
-console.log("Please submit your message to chat-gpt:\n\n");
+console.log("Please submit your prompt to chat-gpt:\n\n");
 
 const stdin = process.openStdin();
 
 // gets called on console input
 stdin.addListener("data", async (data) => {
   const apiKey = process.env.CHAT_GPT_API_KEY;
-  const message = data.toString().trim();
+  const prompt = data.toString().trim();
+  const model = process.env.OPTIONAL_GPT_MODEL;
 
-  const response = await askChatGPTAPI(apiKey, message);
+  const response = await askChatGPTAPI(apiKey, prompt, model);
   console.log(response);
 });
 
 /**
  * @param {string} apiKey
- * @param {string} message
+ * @param {string} prompt
+ * @param {string | undefined} model or default backup if omitted
  * @returns {string}
  */
-async function askChatGPTAPI(apiKey, message) {
+async function askChatGPTAPI(apiKey, prompt, model = "text-davinci-003") {
   const configuration = new Configuration({ apiKey });
   const openai = new OpenAIApi(configuration);
 
-  const completion = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: message,
-  });
+  const completion = await openai.createCompletion({ model, prompt });
 
   return completion.data.choices[0].text;
 }
